21d5a7665a6cc7939701a698f7ed90315e64f209
Uygar <uy.kara@gmail.com>
2020-09-17 17:26:07 +0200

    Refinement in AppController.java closing windows code

---
+++ src/main/java/org/uygar/postit/controllers/application/app/ButtonDisableBinding.java
@@ -0,0 +1,26 @@
+package org.uygar.postit.controllers.application.app;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.Event;
+import javafx.scene.control.Button;
+
+public class ButtonDisableBinding {
+
+    private Button button;
+    private BooleanProperty disablePropertiy = new SimpleBooleanProperty(false);
+
+    public ButtonDisableBinding(Button button) {
+        this.button = button;
+        this.button.disableProperty().bind(disablePropertiy);
+    }
+
+    public void disableOpenWindowButton() {
+        this.disablePropertiy.set(this.disablePropertiy.not().get());
+    }
+
+    public void closedByEventClosed(Event event) {
+        this.disablePropertiy.set(true);
+    }
+
+}
