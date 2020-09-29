790a1134d5c1e9b860ed91b0560bb254f7707143
Uygar <uy.kara@gmail.com>
2020-09-29 08:46:12 +0200

    Added scroll event to post it grid in PostController.java

--- src/main/java/org/uygar/postit/controllers/post/PostController.java
+++ src/main/java/org/uygar/postit/controllers/post/PostController.java
@@ -5,12 +5,17 @@ import javafx.fxml.FXML;
 import javafx.geometry.Dimension2D;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.ScrollEvent;
 import javafx.stage.Stage;
 import org.uygar.postit.data.database.DataMiner;
 import org.uygar.postit.post.Post;
 import org.uygar.postit.post.viewers.post_it.PostItGridViewer;
 
+import javax.net.ssl.SSLPeerUnverifiedException;
+
 public class PostController {
+
+    private static float SCROLL_SPEED = 0.01f;
 
     @FXML
     public TabPane root;
@@ -26,6 +31,7 @@ public class PostController {
         this.postItGrid = new PostItGridViewer(fatherPost, miner);
         this.minDimension = initialWindowDimension;
         initGridPane();
+        this.gridFatherScroll.getContent().setOnScroll(this::setOnScroll);
     }
 
     private void initGridPane() {
@@ -47,6 +53,11 @@ public class PostController {
     private void gridHeightDimensionListener(ObservableValue<? extends Number> obs, Number oldVal, Number newVal) {
         if ((double) newVal < minDimension.getHeight())
             this.root.getScene().getWindow().setHeight(minDimension.getHeight());
+    }
+
+    private void setOnScroll(ScrollEvent event) {
+        double deltaY = event.getDeltaY() * SCROLL_SPEED;
+        gridFatherScroll.setVvalue(gridFatherScroll.getVvalue() - deltaY);
     }
 
 }
